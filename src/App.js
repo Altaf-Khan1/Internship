6614d0552490ce11a1468f3cf62992bb1fc21809
Altaf Khan <Altaf__Khan@outlook.com>
2023-05-18 12:48:11 +0530

    Setting Up the Environment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>!!!Welcome to the World of Coding!!!</p>
+    </div>
+  );
+}
+
+export default App;
