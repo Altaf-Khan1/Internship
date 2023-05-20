69c98f2090ac40caf8c2f3593a304456458b8071
Altaf Khan <Altaf__Khan@outlook.com>
2023-05-20 17:52:15 +0530

    With proper alignement of images and Contact button

--- src/App.js
+++ src/App.js
@@ -16,14 +16,16 @@ function App() {
           <button className="contact">Contact Us</button>
         </div>
         <div class="slide">
-          <img src="https://media.istockphoto.com/id/1182454657/photo/bohemian-living-room-interior-3d-render.jpg?s=612x612&w=0&k=20&c=qw37MGIiTL_jML3_Tbm4bM-jNLCrocSWj7DanhBr_bY=" alt="img">
-          </img>
+          <img src="https://media.istockphoto.com/id/1182454657/photo/bohemian-living-room-interior-3d-render.jpg?s=612x612&w=0&k=20&c=qw37MGIiTL_jML3_Tbm4bM-jNLCrocSWj7DanhBr_bY=" alt="img"></img>
+          <button className="contact">Contact Us</button>
         </div>
         <div class="slide">
           <img src="https://images.unsplash.com/photo-1615873968403-89e068629265?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhvbWUlMjBkZWNvcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt="img"></img>
+          <button className="contact">Contact Us</button>
         </div>
         <div class="slide">
           <img src="https://images.pexels.com/photos/1005058/pexels-photo-1005058.jpeg?cs=srgb&dl=pexels-designecologist-1005058.jpg&fm=jpg" alt="img"></img>
+          <button className="contact">Contact Us</button>
         </div>
       </div>
     </div>
